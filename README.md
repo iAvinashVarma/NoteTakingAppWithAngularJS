5db3fb8bcf2e615fb30a91cbb6e05840c908a4e4
iAvinashVarma <iavinashvarma@gmail.com>
2017-03-06 10:37:59 +0530

    Duplicate #1: Copy and update the code from Code School.

---
+++ README.md
@@ -0,0 +1,2 @@
+# Note Taking App With AngularJS
+This repository contains the code of AngularJS Screencast from Code School course - Note Taking App With AngularJS.
---
+++ app/assets/javascript/routes.js
@@ -0,0 +1,37 @@
+angular.module('NoteWrangler').config(function($routeProvider){
+  $routeProvider
+    .when('/', {
+      redirectTo: '/notes'
+    })
+
+    .when('/notes', {
+      templateUrl: "/../app/assets/templates/notes/index.html",
+      controller: "NotesIndexController"
+    })
+
+    .when('/notes/new', {
+      templateUrl: "/../app/assets/templates/notes/new.html",
+      controller: "NotesCreateController"
+    })
+
+    .when('/notes/:id', {
+      templateUrl: "/../app/assets/templates/notes/show.html",
+      controller: "NotesShowController"
+    })
+
+    .when('/notes/:id/edit', {
+      templateUrl: "/../app/assets/templates/notes/edit.html",
+      controller: "NotesEditController"
+    })
+
+    .when('/users', {
+      templateUrl: "/../app/assets/templates/users/index.html",
+      controller: "UsersIndexController"
+    })
+
+    .when('/users/:id', {
+      templateUrl: "/../app/assets/templates/users/show.html",
+      controller: "UsersShowController"
+    });
+
+});
